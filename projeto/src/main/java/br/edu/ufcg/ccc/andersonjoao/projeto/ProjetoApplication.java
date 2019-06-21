c8b46afd318da4ea64d240533dc1e9f45d43a4fc
Joao <j-m-lima@hotmail.com>
2019-06-21 20:39:49 -0300

    CORS policy fix

--- projeto/src/main/java/br/edu/ufcg/ccc/andersonjoao/projeto/ProjetoApplication.java
+++ projeto/src/main/java/br/edu/ufcg/ccc/andersonjoao/projeto/ProjetoApplication.java
@@ -4,6 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class ProjetoApplication {
@@ -14,6 +17,17 @@ public class ProjetoApplication {
 		filterRb.setFilter(new TokenFilter());
 		filterRb.addUrlPatterns("/private");
 		return filterRb;
+	}
+
+	@Bean
+	public FilterRegistrationBean corsFilter() {
+		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration config = new CorsConfiguration().applyPermitDefaultValues();
+		config.addAllowedMethod("*");
+		source.registerCorsConfiguration("/**", config);
+		FilterRegistrationBean bean = new FilterRegistrationBean(new CorsFilter(source));
+		bean.setOrder(0);
+		return bean;
 	}
 
 	public static void main(String[] args) {
